6b3984e1a3e8dc6e186b97daa091a87d9f7f5f0c
Swapnanil-Gupta <swpnlg@amazon.com>
2026-07-02 15:34:36 -0700

    feat(windows): support windows by converting windows paths to wsl2 paths
    Signed-off-by: Swapnanil-Gupta <swpnlg@amazon.com>

--- option/option_test.go
+++ option/option_test.go
@@ -49,6 +49,136 @@ func TestSupportsEnvVarPassthrough(t *testing.T) {
 	}
 }
 
+func TestTranslateWindowsHostPaths(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		in   []string
+		want []string
+	}{
+		{
+			name: "BuildContextPositional",
+			in:   []string{"build", "-t", "foo", `C:\Users\foo\finch-test123`},
+			want: []string{"build", "-t", "foo", "/mnt/c/Users/foo/finch-test123"},
+		},
+		{
+			name: "BuildDockerfileAndContext",
+			in:   []string{"build", "-f", `D:\a\Dockerfile`, "--no-cache", `D:\a`},
+			want: []string{"build", "-f", "/mnt/d/a/Dockerfile", "--no-cache", "/mnt/d/a"},
+		},
+		{
+			name: "BuildOutputDest",
+			in:   []string{"build", "-t", "output:tag", "--output", `type=tar,dest=C:\Users\foo\out.tar`, `C:\ctx`},
+			want: []string{"build", "-t", "output:tag", "--output", "type=tar,dest=/mnt/c/Users/foo/out.tar", "/mnt/c/ctx"},
+		},
+		{
+			name: "BuildOutputEqualsForm",
+			in:   []string{"build", `--output=type=docker`, `C:\ctx`},
+			want: []string{"build", "--output=type=docker", "/mnt/c/ctx"},
+		},
+		{
+			name: "BuildSecretSrc",
+			in:   []string{"build", "--secret", `id=mysecret,src=C:\Users\foo\secret.txt`, "-f", `C:\Users\foo\Dockerfile`, `C:\Users\foo`},
+			want: []string{
+				"build", "--secret", "id=mysecret,src=/mnt/c/Users/foo/secret.txt",
+				"-f", "/mnt/c/Users/foo/Dockerfile", "/mnt/c/Users/foo",
+			},
+		},
+		{
+			name: "SaveOutputFlag",
+			in:   []string{"save", "-o", `C:\Users\foo\test.tar`, "alpine:latest"},
+			want: []string{"save", "-o", "/mnt/c/Users/foo/test.tar", "alpine:latest"},
+		},
+		{
+			name: "SaveOutputFlagBeforeImages",
+			in:   []string{"save", "--output", `C:\Users\foo\test.tar`, "alpine:latest", "alpine:3.13"},
+			want: []string{"save", "--output", "/mnt/c/Users/foo/test.tar", "alpine:latest", "alpine:3.13"},
+		},
+		{
+			name: "LoadInputFlag",
+			in:   []string{"load", "-i", `C:\Users\foo\test.tar`},
+			want: []string{"load", "-i", "/mnt/c/Users/foo/test.tar"},
+		},
+		{
+			name: "ComposeFileFlag",
+			in:   []string{"compose", "up", "--file", `C:\Users\foo\docker-compose.yml`},
+			want: []string{"compose", "up", "--file", "/mnt/c/Users/foo/docker-compose.yml"},
+		},
+		{
+			name: "CpHostToContainer",
+			in:   []string{"cp", `C:\Users\foo\test-file`, "finch-test-ctr:/tmp/test-file"},
+			want: []string{"cp", "/mnt/c/Users/foo/test-file", "finch-test-ctr:/tmp/test-file"},
+		},
+		{
+			name: "CpContainerToHost",
+			in:   []string{"cp", "finch-test-ctr:/tmp/test-file", `C:\Users\foo\test-file`},
+			want: []string{"cp", "finch-test-ctr:/tmp/test-file", "/mnt/c/Users/foo/test-file"},
+		},
+		{
+			name: "CpWithFollowLinkFlagAndContainerSpec",
+			in:   []string{"cp", "-L", `C:\Users\foo\symlink`, "finch-test-ctr:/tmp/test-file"},
+			want: []string{"cp", "-L", "/mnt/c/Users/foo/symlink", "finch-test-ctr:/tmp/test-file"},
+		},
+		{
+			name: "ExecEnvFileFlag",
+			in:   []string{"exec", "--env-file", `C:\Users\foo\env`, "finch-test-ctr", "env"},
+			want: []string{"exec", "--env-file", "/mnt/c/Users/foo/env", "finch-test-ctr", "env"},
+		},
+		{
+			name: "RunEnvFileFlag",
+			in:   []string{"run", "--rm", "--env-file", `C:\Users\foo\env`, "alpine:latest", "env"},
+			want: []string{"run", "--rm", "--env-file", "/mnt/c/Users/foo/env", "alpine:latest", "env"},
+		},
+		{
+			name: "RunCidFileFlag",
+			in:   []string{"run", "-d", "--cidfile", `C:\Users\foo\test.cid`, "alpine:latest"},
+			want: []string{"run", "-d", "--cidfile", "/mnt/c/Users/foo/test.cid", "alpine:latest"},
+		},
+		{
+			name: "RunLabelFileFlag",
+			in:   []string{"run", "--name", "ctr", "--label-file", `C:\Users\foo\label-file`, "alpine:latest"},
+			want: []string{"run", "--name", "ctr", "--label-file", "/mnt/c/Users/foo/label-file", "alpine:latest"},
+		},
+		{
+			name: "RunBindMountHostPathOnly",
+			in:   []string{"run", "-v", `C:\host:/container`, "alpine:3.13"},
+			want: []string{"run", "-v", "/mnt/c/host:/container", "alpine:3.13"},
+		},
+		{
+			name: "RunNamedVolumeUntouched",
+			in:   []string{"run", "-v", "foo:/usr/share", "--name", "ctr", "alpine:3.13"},
+			want: []string{"run", "-v", "foo:/usr/share", "--name", "ctr", "alpine:3.13"},
+		},
+		{
+			name: "RunAnonymousVolumeUntouched",
+			in:   []string{"run", "-v", "/usr/share", "--name", "ctr", "alpine:3.13"},
+			want: []string{"run", "-v", "/usr/share", "--name", "ctr", "alpine:3.13"},
+		},
+		{
+			name: "PullImageTagAndFlagsUntouched",
+			in:   []string{"pull", "alpine:3.13", "--platform", "linux/amd64"},
+			want: []string{"pull", "alpine:3.13", "--platform", "linux/amd64"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := translateWindowsHostPaths(test.in)
+			if len(got) != len(test.want) {
+				t.Fatalf("length mismatch: got %v, want %v", got, test.want)
+			}
+			for i := range got {
+				if got[i] != test.want[i] {
+					t.Errorf("arg %d: got %q, want %q", i, got[i], test.want[i])
+				}
+			}
+		})
+	}
+}
+
 func TestNerdctlVersion(t *testing.T) {
 	t.Parallel()
 
